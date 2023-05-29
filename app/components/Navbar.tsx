252cfedcf1cafb775ff1219869186d8e1572c487
Espetec <manutheking1991@gmail.com>
2023-05-30 00:42:20 +0200

    css changes, sidebar somewhat nice, zzz

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,25 @@
 import '../globals.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-800 dark:border-gray-700 sticky top-0">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
+        <nav className="bg-white border-gray-200 dark:bg-gray-800 dark:border-gray-700 sticky top-0 ">
+            <div className="w-full flex flex-wrap items-center justify-between p-4">
                 <Link href="#" className="flex items-center">
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MANUCOMICS</span>
                 </Link>
                 
-                <div className="flex items-center place-content-end">
-                    <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                <div className="relative flex items-center">
+                    <button type="button" className="relative text-sm bg-gray-800 rounded-full  focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                         <span className="sr-only">Open user menu</span>
-                        <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo"/>
+                        <Image
+                            className='rounded-full'
+                            src="/pochita_avatar.png"
+                            width={48}
+                            height={48}
+                            alt="avatar"
+                        />
                     </button>
                     {/* Dropdown menu */}
                     <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
@@ -22,23 +29,19 @@ export default function Navbar() {
                         </div>
                         <ul className="py-2" aria-labelledby="user-menu-button">
                         <li>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                            <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</Link>
                         </li>
                         <li>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                            <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</Link>
                         </li>
                         <li>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                            <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</Link>
                         </li>
                         <li>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                            <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</Link>
                         </li>
                         </ul>
                     </div>
-                    <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                    </button>
                 </div>
             </div>
         </nav>
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -4,6 +4,9 @@ import React, { ReactNode } from "react";
 import HomeIcon from '@mui/icons-material/Home';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import ArchiveIcon from '@mui/icons-material/Archive';
+import InboxIcon from '@mui/icons-material/Inbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { AppBar, Box, CssBaseline, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material";
 
 export default function Sidebar() {
 
@@ -24,19 +27,62 @@ export default function Sidebar() {
         {icon: <ArchiveIcon/>, content: "Local Inventory", link: '/inventory'},
     ];
 
+    const drawerWidth = 240;
+
     return (
-        <div className="flex flex-col justify-between bg-indigo-700 text-zinc-50  md:sticky md:top-16 md:z-0 top-0 z-20 fixed md:h-[calc(100vh_-_64px)] h-full w-[300px] transition-transform .3s ease-in-out md:translate-x-0-translate-x-full">
-        <nav className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-            {/* nav items */}
-            <ul className="space-y-2 font-medium">
-                {sidebarOptions.map(l => {
-                    return (
-                        listButton(l)
-                    )
-                })
-                }
-            </ul>
-        </nav>
-        </div>
+        // <div className="flex flex-col justify-between bg-indigo-700 text-zinc-50  md:sticky md:top-16 md:z-0 top-0 z-20 fixed md:h-[calc(100vh_-_64px)] h-full w-[300px] transition-transform .3s ease-in-out md:translate-x-0-translate-x-full">
+        // <nav className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        //     {/* nav items */}
+        //     <ul className="space-y-2 font-medium">
+        //         {sidebarOptions.map(l => {
+        //             return (
+        //                 listButton(l)
+        //             )
+        //         })
+        //         }
+        //     </ul>
+        // </nav>
+        // </div>
+        <Box sx={{ display: 'flex'}}>
+            {/* <CssBaseline /> */}
+            <Drawer
+                variant="permanent"
+                className="bg-gray-50 dark:bg-gray-800"
+                sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box', top: 'auto' },
+                }}
+                
+            >
+                <Box sx={{ overflow: 'auto' }}>
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                    ))}
+                </List>
+                {/* <Divider />
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                    ))}
+                </List> */}
+                </Box>
+            </Drawer>
+            </Box>
     );
 }
